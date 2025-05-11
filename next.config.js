2aa6fb1328fdaba893c39691686428cfde3d9363
iChaeYeong <chae_young813@naver.com>
2025-05-11 17:01:18 +0900

    fix: next.config.js 설정값 변경

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'export',
     images: {
+        unoptimized: true,
         domains: ['via.placeholder.com'],
     },
+    trailingSlash: true,
 };
 
 module.exports = nextConfig; 
